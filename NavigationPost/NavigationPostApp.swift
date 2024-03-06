7766642294b4bc942d2f94e655ab158e08b1ccce
AyselPro <AyselLuckyPro@gmail.comgit config --global user.email AyselLuckyPro@gmail.com>
2024-03-06 23:00:41 +0300

    Initial Commit

---
+++ NavigationPost/NavigationPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPostApp.swift
+//  NavigationPost
+//
+//  Created by Aysel on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
